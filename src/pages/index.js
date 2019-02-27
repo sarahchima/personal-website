a7db2c6d413dce053fb9914f3b433b9c62166587
Sarah Chima <sarahchimao@gmail.com>
2019-02-27 20:46:47 +0100

    words change

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default () => (
             </picture> 
             <article className="ap-text">
                 <h1 className="ap-header">Hi, I'm Sarah.</h1>
-                <p>I am a <strong>Frontend Developer</strong> from Nigeria. I am a strong advocate of responsive and accessible websites. I love challenges and I believe facing new challenges is the best way to grow as an Engineer.</p>
+                <p>I am a <strong>Front-End Developer</strong> from Nigeria. I am a strong advocate of responsive and accessible websites. I love challenges and I believe facing new challenges is the best way to grow as an Engineer.</p>
                 <p>When I am not coding, I write blog posts on my <Link to={`/blog`} className="bold"> blog </Link> or on <a href="https://www.instagram.com/sarah_codes_/" className="bold">Instagram</a>. These articles cover a wide range of Frontend topics including React, ES6, CSS, and SASS. I love designing user interfaces with Figma and applying principles of UX.</p>
                 <p>I also love cooking, listening to good music and sharing the good news with people.</p>
             </article>
