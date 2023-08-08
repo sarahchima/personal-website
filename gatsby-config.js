06c33013401e0a2317230a4709f247b78092efe3
Sarah Chima <sarahchimao@gmail.com>
2023-08-08 04:37:35 +0200

    Up to date about section; Removed irrelevant project section

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,42 @@
 module.exports = {
-    siteMetadata: {
-      title: `Sarah Chima - Front-End Developer`,
-      description: `Front-End Developer and Technical Writer from Nigeria.`,
-      siteUrl: `https://www.gatsbyjs.org`,
-      author: `Sarah Chima`,
-      defaultImage: `https://res.cloudinary.com/dvj2hbywq/image/upload/v1550930072/potrait.jpg`
+  siteMetadata: {
+    title: `Sarah Chima Atuonwu- Software Engineer`,
+    description: `Software engineer based in Stockholm, Sweden.`,
+    siteUrl: `https://www.gatsbyjs.org`,
+    author: `Sarah Chima Atuonwu`,
+    defaultImage: `https://res.cloudinary.com/dvj2hbywq/image/upload/v1550930072/potrait.jpg`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
     },
-    plugins: [
-        {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-            name: `src`,
-            path: `${__dirname}/src/`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-126807182-1",
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
           },
-        },
-        `gatsby-plugin-react-helmet`,
-        {
-          resolve: `gatsby-plugin-google-analytics`,
-          options: {
-            trackingId: "UA-126807182-1",
-          },
-        },
-        {
-          resolve: `gatsby-transformer-remark`,
-          options: {
-            plugins: [{
-              resolve: `gatsby-remark-prismjs`,
-              options: {
-                classPrefix: "language-",
-                inlineCodeMarker: null,
-                aliases: {},
-                showLineNumbers: false,
-                noInlineHighlight: false,
-              },
-            }],
-          },
-        },
-    ],
-  }
-  
+        ],
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,50 @@
-import React from "react"
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import Potrait from '../images/potrait2.jpg'
+import Potrait from "../images/potrait2.jpg";
 
 export default () => (
-    <Layout>
-        <SEO 
-            title={` `}
-            keywords={[`Front-End Engineer`, `UI`, `Developer`, `CSS`, `JavaScript`]}
-            description={`Welcome to Sarah Chima's website`}
-        />
-        <div className="ap">
-            <picture className="ap-picture">
-                <img className="ap-img" src={Potrait} alt="Sarah" />
-            </picture> 
-            <article className="ap-text">
-                <h1 className="ap-header">Hi, I'm Sarah.</h1>
-                <p>I am a <strong>Front-End Developer</strong> from Nigeria. I am a strong advocate of responsive and accessible websites. I love challenges and I believe facing new challenges is the best way to grow as an Engineer.</p>
-                <p>When I am not coding, I write blog posts on my <Link to={`/blog`} className="bold"> blog </Link> or on <a href="https://www.instagram.com/sarah_codes_/" className="bold">Instagram</a>. These articles cover a wide range of Frontend topics including React, ES6, CSS, and SASS. I love designing user interfaces with Figma and applying principles of UX.</p>
-                <p>I also love cooking, listening to good music and sharing the good news with people.</p>
-            </article>
-        </div>
-    </Layout>
-)
+  <Layout>
+    <SEO
+      title={` `}
+      keywords={[`Software Engineer`, `UI`, `Developer`, `CSS`, `JavaScript`]}
+      description={`Welcome to Sarah Chima Atuonwu's website`}
+    />
+    <div className="ap">
+      <picture className="ap-picture">
+        <img className="ap-img" src={Potrait} alt="Sarah" />
+      </picture>
+      <article className="ap-text">
+        <h1 className="ap-header">Hi, I'm Sarah.</h1>
+        <p>
+          I am a <strong>Software Engineer</strong> based in Stockholm with 5+
+          years of experience building tech products.{" "}
+        </p>
+        <p>
+          I love sharing what I learn so I write on my{" "}
+          <Link to={`/blog`} className="bold">
+            {" "}
+            blog
+          </Link>
+          ,
+          <Link
+            to={`https://blog.logrocket.com/author/sarahchima/`}
+            className="bold"
+          >
+            {" "}
+            logrocket
+          </Link>{" "}
+          or on{" "}
+          <a href="https://www.instagram.com/sarah_codes_/" className="bold">
+            Instagram
+          </a>
+          . These articles cover a wide range of topics including React, ES6,
+          CSS, and SASS.
+        </p>
+      </article>
+    </div>
+  </Layout>
+);
